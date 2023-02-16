199aeee8055023e48c076976033631f82c8545e2
badrisaiprasad <88886294+badrisaiprasad@users.noreply.github.com>
2023-02-16 15:15:13 +0530

    map

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'cluster-map',
-    style: 'mapbox://styles/mapbox/light-v10',
+    style: 'mapbox://styles/mapbox/navigation-day-v1',
     center: [79.947998, 22.832],
     zoom: 3.3
 });
